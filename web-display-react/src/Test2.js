b52faa2383c35c3b46fb81db66c4f1af8c007e2c
morgan-bredford <71643839+morgan-bredford@users.noreply.github.com>
2020-09-23 21:16:17 +0200

    Create Test2.js

---
+++ web-display-react/src/Test2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test2(){
+    return(
+        <React.Fragment>
+            <h1>react test 2</h1>
+        </React.Fragment>
+    )
+}
+
+export default Test2
