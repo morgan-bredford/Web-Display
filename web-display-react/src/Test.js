b3de5d7d6bdf378613d4bc2d4c0ff6d1b86d48c2
morgan-bredford <morgan.bredford@gmail.com>
2020-09-23 21:13:57 +0200

    create Test
    testing Test

---
+++ web-display-react/src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test(){
+    return(
+        <React.Fragment>
+            <h1>react test</h1>
+        </React.Fragment>
+    )
+}
+
+export default Test
--- web-display-react/src/index.js
+++ web-display-react/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Test from './Test'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Test />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
