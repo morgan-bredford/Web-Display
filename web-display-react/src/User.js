12fc120e35e9844c7a4fcfd8ec33c29f08c7a9c8
morgan-bredford <morgan.bredford@gmail.com>
2020-09-24 12:33:32 +0200

    axios stuff
    getting axios going

---
+++ web-display-react/src/User.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      gender: this.props.gender,
+      height: this.props.height,
+    };
+    // this.state.name = this.props.name;
+    // this.state.gender = this.props.gender;
+    // this.state.height = this.props.height;
+  }
+  componentDidMount() {}
+  render() {
+    return (
+      <div>
+        name: {this.state.name} <br />
+        gender: {this.state.gender} <br />
+        height: {this.state.height} <br />
+      </div>
+    );
+  }
+}
+
+export default User;
--- web-display-react/src/index.js
+++ web-display-react/src/index.js
@@ -2,9 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Test from "./Test";
+import User from "./User";
+import axios from "axios";
+
+// async function getUsers() {
+//   try {
+//     const res = await axios.get(
+//       "https://swapi.dev/api/people/?search=skywalker"
+//     );
+//     console.log(typeof res.data);
+//     res.map((user) => {
+//       // return (
+//       //   <User name={user.name} gender={user.gender} heigth={user.height} />
+//       // );
+//       console.log(user);
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+// function getUsers() {
+//   axios.get("https://swapi.dev/api/people/?search=skywalker").then((res) => {
+//     console.log(res.data);
+//   });
+// }
+function getUsers() {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos?_limit=3")
+    .then((res) => {
+      console.log(res.data);
+    });
+}
+getUsers();
 
 ReactDOM.render(
   <React.StrictMode>
+    getUsers();
     <Test />
     <App />
   </React.StrictMode>,
