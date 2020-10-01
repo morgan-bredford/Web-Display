b0eb358cdbb9742f3b73326f6a30d58f93514487
morgan-bredford <morgan.bredford@gmail.com>
2020-10-02 00:47:32 +0200

    Update UsersClass.js
    gjgj

--- web-display-react/src/UsersClass.js
+++ web-display-react/src/UsersClass.js
@@ -31,7 +31,8 @@ class UsersClass extends Component {
   nodetest() {
     axios
       .get("http://127.0.0.1:5000/?test=test")
-      .then((res) => console.log(res));
+      .then((res) => console.log(res))
+      .catch(err => console.log(err))
     // .then(console.log(this.state.users));
   }
 
