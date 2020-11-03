cc409c885cefc581603ee6950e78486b12379273
morgan-bredford <morgan.bredford@gmail.com>
2020-11-03 12:38:17 +0100

    reyery

---
+++ web-display-vue/src/components/BuildGallery.vue
@@ -0,0 +1,32 @@
+<template>
+  <SearchImages @addimg="addImage" />
+  <div className='imagecontainer' v-if="saved_images.length">
+      <span v-for="img in saved_images" :key="img.id">
+          <img :src=img.previewURL />
+      </span>
+  </div>
+</template>
+
+<script>
+import SearchImages from './SearchImages.vue'
+
+export default {
+    name: "BuildGallery",
+    components: {SearchImages},
+    data(){
+        return{
+            saved_images: [],
+        }
+    },
+    methods: {
+        addImage(img){
+            this.saved_images.push(img)
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
