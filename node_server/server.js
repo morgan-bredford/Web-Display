ae20e9ed1c7909e557e84ae224cc501ba822c56f
morgan-bredford <morgan.bredford@gmail.com>
2020-12-09 12:43:05 +0100

    tryujtj

--- node_server/server.js
+++ node_server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const multer = require('multer')
+const path = require('path')
 
 require("dotenv").config();
 
@@ -17,14 +19,31 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
 
 app.post("/test", (req,res) => {
   res.header("Access-Control-Allow-Origin","*")
   res.send('send success')
 })
+
+app.use(express.static('./public'))
+
+const storage = multer.diskStorage({
+  destination: '.public/uploads/',
+  filename: (req, file, cb) => {
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('inpFile');
--- web-display-react/src/ImageTest.js
+++ web-display-react/src/ImageTest.js
@@ -17,20 +17,38 @@ class ImageTest extends React.Component {
         const formData = new FormData()
         formData.append("inpFile", inpFile.files[0])
         axios
-        .post(endpoint,formData)
+        .post('http://127.0.0.1:8080/users/image',formData)
                 .catch(err => {
                     console.log(err.response)})
-        // const file = event.target.files[0]
-        // this.image = file
-        // this.setState({imageUrl: URL.createObjectURL(file)})
+        const file = event.target.files[0]
+        this.image = file
+        this.setState({imageUrl: URL.createObjectURL(file)})
     }
 
+    uploadImage2 = (e) => {
+        const inpFile = e.target.files[0]
+        this.readFile(inpFile,this.cb)
+    }
+    readFile = (file, cb) => {
+        let myReader = new FileReader();
+        myReader.onloadend = function (e) {
+            cb(myReader.result);
+        };
+        myReader.readAsDataURL(file);
+    };
+   cb = (base64string) => {
+        /*do next steps here like sending image base64string to the server.*/
+        axios
+        .post('http://127.0.0.1:8080/users/image',base64string)
+                .catch(err => {
+                    console.log(err.response)})
+    }
     render() {
         return(
             <React.Fragment>
                 <br /><br />
                     <label htmlFor='file'>upload</label>
-                    <input type="file" accept="image/*" name="image" id="file" onChange={this.uploadImage} style={{display: 'none'}} />
+                    <input type="file" accept="image/*" name="image" id="file" onChange={this.uploadImage2} style={{display: 'none'}} />
                 <img src={this.state.imageUrl} />
             </React.Fragment>
         )
